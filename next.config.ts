b2b77fb69dd30ed030f8c78314e10d614e5e4473
Eshan05 <nahareshan05@gmail.com>
2026-07-01 13:10:17 +0530

    Next but deploying on Lemma

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/lemma-api/:path*",
-        destination: "https://api.lemma.work/:path*",
-      },
-    ]
-  },
+  output: "export",
 }
 
 export default nextConfig
